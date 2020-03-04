b78aa017cb1a5dc5f092468d9fa7425a0c83c6a4
ethan <1589664970@qq.com>
2020-03-04 20:07:52 +0800

    update: framework net

--- framework/src/app/scenes/MainScene.lua
+++ framework/src/app/scenes/MainScene.lua
@@ -9,7 +9,16 @@ end
 function MainScene:onEnter()
 end
 
+function MainScene:onEnterTransitionFinish()
+end
+
+function MainScene:onExitTransitionStart()
+end
+
 function MainScene:onExit()
+end
+
+function MainScene:onCleanup()
 end
 
 return MainScene
---
+++ framework/src/net/protoloader.lua
@@ -0,0 +1,15 @@
+import(".protobuf")
+local parser = import(".parser")
+
+local protoloader = {}
+
+function protoloader.encodeMsg(name, msg)
+end
+
+function protoloader.decodeMsg(msgid, data)
+end
+
+function protoloader.loadProto()
+end
+
+return protoloader
